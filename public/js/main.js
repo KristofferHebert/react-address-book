99d6c866478e6bdaf597c5257010d6a4c09dc26f
= <kristoffer@kristofferhebert.com>
2015-11-11 16:11:41 -0800

    tweaking styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,219 +1,293 @@
 'use strict';
 
+// React AddressContainer. Responsible for fetching data.
+
 var AddressContainer = React.createClass({
-    getInitialState: function getInitialState() {
-        return {
-            addresses: []
-        };
-    },
-    componentDidMount: function componentDidMount() {
-        var request = new XMLHttpRequest();
-        var self = this;
+  getInitialState: function getInitialState() {
+    return {
+      addresses: [],
+      total: ''
+    };
+  },
+  componentDidMount: function componentDidMount() {
+    // check cache for cached response
+    var cacheAddresses = localStorage.getItem('addresses');
+    if (cacheAddresses) {
+      console.log('using local storage');
+      var cacheAddresses = JSON.parse(cacheAddresses);
+      this.setState({
+        addresses: cacheAddresses,
+        total: cacheAddresses.length
+      });
+    } else {
+      console.log('fetching data');
+      var request = new XMLHttpRequest();
+      var self = this;
 
-        // when request returns a result set state
-        request.onreadystatechange = function () {
-            if (request.readyState >= 4 && request.status >= 200) {
-                var addresses = JSON.parse(request.responseText);
-                if (self.isMounted()) {
-                    self.setState({ addresses: addresses.data });
-                }
-            }
-        };
+      // when request returns a result set state
+      request.onreadystatechange = function () {
+        if (request.readyState >= 4 && request.status >= 200) {
+          var addresses = JSON.parse(request.responseText);
+          addresses = addresses.data;
+          localStorage.setItem('addresses', JSON.stringify(addresses));
 
-        // configure method and url via props.data string
-        request.open("GET", this.props.data);
-        request.send(null);
-    },
-
-    render: function render() {
-        return React.createElement(
-            "div",
-            null,
-            React.createElement(AddressPage, { addresses: this.state.addresses })
-        );
-    }
-});
-
-var AddressPage = React.createClass({
-    handlePageChange: function handlePageChange(page) {
-        var newPage = this.state.page + page < 1 ? 1 : Number(this.state.page) + page;
-        this.setState({
-            page: newPage
-        });
-    },
-    handlePageSizeChange: function handlePageSizeChange(event) {
-        this.setState({
-            pageSize: Number(event.target.value)
-        });
-    },
-    populateAddresses: function populateAddresses(addressArray) {
-        var end = this.state.page * this.state.pageSize - 1;
-        var index = end - this.state.pageSize;
-        index = this.state.page === 1 ? -1 : index;
-
-        console.log(this.state, index, end);
-
-        var result = Array(end);
-        while (index++ < end) {
-            result[index] = addressArray[index];
+          if (self.isMounted()) {
+            self.setState({
+              addresses: addresses,
+              total: addresses.length
+            });
+          }
         }
-        return result;
-    },
-    getInitialState: function getInitialState() {
-        return {
-            page: 1,
-            pageSize: 5,
-            pageSizeDefaults: [5, 10, 25, 50, 75, 100]
-        };
-    },
-    render: function render() {
-        var addresses = this.populateAddresses(this.props.addresses);
-        return React.createElement(
-            "div",
-            null,
-            React.createElement(AddressPageDropdown, { pageSizeDefaults: this.state.pageSizeDefaults, handleChange: this.handlePageSizeChange, page: this.state.page, className: "fl" }),
-            React.createElement(Button, { className: "button", name: "<< Previous", handleClick: this.handlePageChange.bind(null, -1) }),
-            React.createElement(Button, { className: "button", name: "Next >>", handleClick: this.handlePageChange.bind(null, 1) }),
-            React.createElement(AddressList, { addresses: addresses })
-        );
+      };
+
+      // configure method and url via props.data string
+      request.open("GET", this.props.data);
+      request.send(null);
     }
+  },
+
+  render: function render() {
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(AddressPage, {
+        addresses: this.state.addresses,
+        total: this.state.total })
+    );
+  }
 });
 
+// Renders Address page
+var AddressPage = React.createClass({
+  handlePageChange: function handlePageChange(page) {
+
+    var newPage;
+
+    if (this.state.page === this.state.numberOfPages) {
+      newPage = this.state.page + page > this.state.numberOfPages ? this.state.numberOfPages : Number(this.state.page) + page;
+    } else {
+      newPage = this.state.page + page < 1 ? 1 : Number(this.state.page) + page;
+    }
+
+    this.setState({ page: newPage });
+  },
+  handlePageSizeChange: function handlePageSizeChange(event) {
+
+    var newSize = Number(event.target.value);
+    var numberOfPages = Math.floor(Number(this.props.total) / newSize);
+
+    this.setState({
+      page: 1,
+      pageSize: newSize,
+      numberOfPages: numberOfPages
+    });
+  },
+  populateAddresses: function populateAddresses(addressArray) {
+    var end = this.state.page * this.state.pageSize - 1;
+    var index = end - this.state.pageSize;
+    index = this.state.page === 1 ? -1 : index;
+
+    var result = Array(end);
+    while (index++ < end) {
+      result[index] = addressArray[index];
+    }
+    return result;
+  },
+  getInitialState: function getInitialState() {
+    return {
+      page: 1,
+      pageSize: 5,
+      pageSizeDefaults: [5, 10, 25, 50, 75, 100],
+      numberOfPages: 20
+    };
+  },
+  updateNumberOfPages: function updateNumberOfPages() {
+    this.setState({ numberOfPages: numberOfPages });
+  },
+  render: function render() {
+    var addresses = this.populateAddresses(this.props.addresses);
+    return React.createElement(
+      'section',
+      null,
+      React.createElement(
+        'div',
+        { className: 'cb' },
+        React.createElement(Button, {
+          className: 'button fl mr',
+          name: '<< Previous',
+          handleClick: this.handlePageChange.bind(null, -1) }),
+        React.createElement(
+          'p',
+          { className: 'count' },
+          'Page',
+          this.state.page,
+          ' of',
+          this.state.numberOfPages
+        ),
+        React.createElement(Button, {
+          className: 'button fl mr',
+          name: 'Next >>',
+          handleClick: this.handlePageChange.bind(null, 1) }),
+        React.createElement(AddressPageDropdown, {
+          pageSizeDefaults: this.state.pageSizeDefaults,
+          handleChange: this.handlePageSizeChange,
+          page: this.state.page,
+          className: 'fl mr cr' })
+      ),
+      React.createElement(
+        'div',
+        { className: 'cb' },
+        React.createElement(AddressList, { addresses: addresses })
+      )
+    );
+  }
+});
+
+// Renders Button. Controls page of addresses.
 var Button = React.createClass({
-    render: function render() {
-        return React.createElement(
-            "a",
-            { href: "#", className: this.props.className, onClick: this.props.handleClick },
-            this.props.name
-        );
-    }
+  render: function render() {
+    return React.createElement(
+      'a',
+      {
+        href: '#',
+        className: this.props.className,
+        onClick: this.props.handleClick },
+      this.props.name
+    );
+  }
 });
 
+// Renders Address page dropdown. Controls number of addresses displayed at once.
 var AddressPageDropdown = React.createClass({
-    getDefaultProps: function getDefaultProps() {
-        return {
-            pageSizeDefaults: null,
-            page: null
-        };
-    },
-    render: function render() {
-        var options = this.props.pageSizeDefaults.map(function (value, index) {
-            return React.createElement(
-                "option",
-                { value: value, key: index },
-                value,
-                " items"
-            );
-        });
+  getDefaultProps: function getDefaultProps() {
+    return { pageSizeDefaults: null, page: null };
+  },
+  render: function render() {
+    var options = this.props.pageSizeDefaults.map(function (value, index) {
+      return React.createElement(
+        'option',
+        {
+          value: value,
+          key: index },
+        value,
+        ' items'
+      );
+    });
 
-        return React.createElement(
-            "div",
-            null,
-            React.createElement(
-                "select",
-                { onChange: this.props.handleChange, className: "this.pros.className" },
-                options
-            )
-        );
-    }
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'select',
+        {
+          onChange: this.props.handleChange,
+          className: this.props.className },
+        options
+      )
+    );
+  }
 });
 
+// Renders List of Addresses
 var AddressList = React.createClass({
-    getDefaultProps: function getDefaultProps() {
-        return {
-            addresses: []
-        };
-    },
-    render: function render() {
-        var AddressTable = this.props.addresses.map(function (address, index) {
+  getDefaultProps: function getDefaultProps() {
+    return {
+      addresses: []
+    };
+  },
+  render: function render() {
+    var AddressTable = this.props.addresses.map(function (address, index) {
 
-            return React.createElement(AddressItem, { address: address, key: index });
-        });
-        return React.createElement(
-            "table",
+      return React.createElement(AddressItem, {
+        address: address,
+        key: index });
+    });
+    return React.createElement(
+      'table',
+      null,
+      React.createElement(
+        'tbody',
+        null,
+        React.createElement(
+          'tr',
+          null,
+          React.createElement(
+            'th',
             null,
-            React.createElement(
-                "tbody",
-                null,
-                React.createElement(
-                    "tr",
-                    null,
-                    React.createElement(
-                        "th",
-                        null,
-                        "First Name"
-                    ),
-                    React.createElement(
-                        "th",
-                        null,
-                        "Last Name"
-                    ),
-                    React.createElement(
-                        "th",
-                        null,
-                        "Street"
-                    ),
-                    React.createElement(
-                        "th",
-                        null,
-                        "State"
-                    ),
-                    React.createElement(
-                        "th",
-                        null,
-                        "Zip"
-                    )
-                ),
-                AddressTable
-            )
-        );
-    }
+            'First Name'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Last Name'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Street'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'State'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Zip'
+          )
+        ),
+        AddressTable
+      )
+    );
+  }
 });
 
+// Renders individual Addresses
 var AddressItem = React.createClass({
-    getDefaultProps: function getDefaultProps() {
-        return {
-            address: {
-                City: "Loading...",
-                FirstName: "Loading...",
-                LastName: "Loading...",
-                State: "Loading...",
-                Street: "Loading...",
-                Zip: "Loading..."
-            }
-        };
-    },
-    render: function render() {
-        return React.createElement(
-            "tr",
-            null,
-            React.createElement(
-                "td",
-                null,
-                this.props.address.FirstName
-            ),
-            React.createElement(
-                "td",
-                null,
-                this.props.address.LastName
-            ),
-            React.createElement(
-                "td",
-                null,
-                this.props.address.Street
-            ),
-            React.createElement(
-                "td",
-                null,
-                this.props.address.State
-            ),
-            React.createElement(
-                "td",
-                null,
-                this.props.address.Zip
-            )
-        );
-    }
+  getDefaultProps: function getDefaultProps() {
+    return {
+      address: {
+        City: "Loading...",
+        FirstName: "Loading...",
+        LastName: "Loading...",
+        State: "Loading...",
+        Street: "Loading...",
+        Zip: "Loading..."
+      }
+    };
+  },
+  render: function render() {
+    return React.createElement(
+      'tr',
+      null,
+      React.createElement(
+        'td',
+        null,
+        this.props.address.FirstName
+      ),
+      React.createElement(
+        'td',
+        null,
+        this.props.address.LastName
+      ),
+      React.createElement(
+        'td',
+        null,
+        this.props.address.Street
+      ),
+      React.createElement(
+        'td',
+        null,
+        this.props.address.State
+      ),
+      React.createElement(
+        'td',
+        null,
+        this.props.address.Zip
+      )
+    );
+  }
 });
 
-React.render(React.createElement(AddressContainer, { data: "/api/address" }), document.getElementById('app'));
+React.render(React.createElement(AddressContainer, { data: '/api/address' }), document.getElementById('app'));
--- src/js/main.js
+++ src/js/main.js
@@ -1,167 +1,267 @@
 'use strict'
 
-var AddressContainer = React.createClass({
-    getInitialState: function(){
-        return {
-          addresses: []
+// React AddressContainer. Responsible for fetching data.
+var AddressContainer = React.createClass( {
+  getInitialState: function () {
+    return {
+      addresses: [],
+      total: ''
+    }
+  },
+  componentDidMount() {
+    // check cache for cached response
+    var cacheAddresses = localStorage.getItem( 'addresses' )
+    if ( cacheAddresses ) {
+      console.log( 'using local storage' )
+      var cacheAddresses = JSON.parse( cacheAddresses )
+      this.setState( {
+        addresses: cacheAddresses,
+        total: cacheAddresses.length
+      } )
+    } else {
+      console.log( 'fetching data' )
+      var request = new XMLHttpRequest()
+      var self = this
+
+      // when request returns a result set state
+      request.onreadystatechange = function () {
+        if ( request.readyState >= 4 && request.status >= 200 ) {
+          var addresses = JSON.parse( request.responseText )
+          addresses = addresses.data
+          localStorage.setItem( 'addresses', JSON.stringify( addresses ) )
+
+          if ( self.isMounted() ) {
+            self.setState( {
+              addresses: addresses,
+              total: addresses.length
+            } )
+          }
+        }
       }
-    },
-    componentDidMount(){
-            var request = new XMLHttpRequest()
-            var self = this
 
-    	// when request returns a result set state
-            request.onreadystatechange = function(){
-                if(request.readyState >= 4 && request.status >= 200){
-                    var addresses = JSON.parse(request.responseText)
-                    if(self.isMounted()){
-                        self.setState({addresses: addresses.data})
-                    }
-                }
-            }
-
-    	// configure method and url via props.data string
-            request.open("GET", this.props.data)
-            request.send(null)
-
-        },
-        render: function(){
-            return (
-                <div>
-                    <AddressPage addresses={this.state.addresses}/>
-                </div>
-            )
-        }
-})
-
-var AddressPage = React.createClass({
-    handlePageChange: function(page){
-            var newPage = (this.state.page + page < 1) ? 1 : (Number(this.state.page) + page)
-            this.setState({
-                page: newPage
-            })
-    },
-    handlePageSizeChange: function(event){
-            this.setState({
-                pageSize: Number(event.target.value)
-            })
-    },
-    populateAddresses: function(addressArray){
-        var end = this.state.page * this.state.pageSize -1
-        var index = end - this.state.pageSize
-        index = (this.state.page === 1) ? -1 : index
-
-        console.log(this.state,index, end)
-
-        var result = Array(end)
-            while(index++ < end){
-                result[index] = addressArray[index]
-            }
-            return result
-    },
-    getInitialState: function(){
-        return {
-            page: 1,
-            pageSize: 5,
-            pageSizeDefaults: [5,10,25,50,75,100]
-        }
-    },
-    render: function(){
-            var addresses = this.populateAddresses(this.props.addresses)
-            return (
-                <div>
-                    <AddressPageDropdown pageSizeDefaults={this.state.pageSizeDefaults} handleChange={this.handlePageSizeChange} page={this.state.page}  className="fl"/>
-                    <Button className="button" name="<< Previous" handleClick={this.handlePageChange.bind(null, -1)} />
-                    <Button className="button" name="Next >>" handleClick={this.handlePageChange.bind(null, 1)} />
-                    <AddressList addresses={addresses} />
-                </div>
-            )
+      // configure method and url via props.data string
+      request.open( "GET", this.props.data )
+      request.send( null )
     }
-})
+  },
+  render: function () {
+    return (
+    <div>
+      <AddressPage
+                   addresses={ this.state.addresses }
+                   total={ this.state.total } />
+    </div>
+    )
+  }
+} )
 
-var Button = React.createClass({
-    render: function(){
-        return (
-            <a href="#" className={this.props.className} onClick={this.props.handleClick}>{this.props.name}</a>
-        )
+
+// Renders Address page
+var AddressPage = React.createClass( {
+  handlePageChange: function ( page ) {
+
+    var newPage
+
+    if ( this.state.page === this.state.numberOfPages ) {
+      newPage = (this.state.page + page > this.state.numberOfPages) ? this.state.numberOfPages : (Number( this.state.page ) + page)
+    } else {
+      newPage = (this.state.page + page < 1) ? 1 : (Number( this.state.page ) + page)
     }
-})
 
-var AddressPageDropdown = React.createClass({
-    getDefaultProps: function() {
-        return {
-            pageSizeDefaults: null,
-            page: null
-        }
-    },
-    render: function(){
-        var options = this.props.pageSizeDefaults.map(function(value, index){
-            return <option value={value} key={index}>{value} items</option>
-        })
+    this.setState( { page: newPage } )
+  },
+  handlePageSizeChange: function ( event ) {
 
-        return(
-            <div>
-                <select onChange={this.props.handleChange} className="this.pros.className">{options}</select>
-            </div>
-        )
+    var newSize = Number( event.target.value )
+    var numberOfPages = Math.floor( Number( this.props.total ) / newSize )
+
+    this.setState( {
+      page: 1,
+      pageSize: newSize,
+      numberOfPages: numberOfPages
+    } )
+  },
+  populateAddresses: function ( addressArray ) {
+    var end = this.state.page * this.state.pageSize - 1
+    var index = end - this.state.pageSize
+    index = (this.state.page === 1) ? -1 : index
+
+    var result = Array( end )
+    while (index++ < end) {
+      result[ index ] = addressArray[ index ]
     }
-})
-
-var AddressList = React.createClass({
-    getDefaultProps: function(){
-        return {
-            addresses: []
-        }
-    },
-    render: function(){
-            var AddressTable = this.props.addresses.map(function(address, index){
-
-                return <AddressItem address={address} key={index}/>
-            })
-        return (
-            <table>
-                 <tbody>
-                <tr>
-                    <th>First Name</th>
-                <th>Last Name</th>
-            <th>Street</th>
-        <th>State</th>
-    <th>Zip</th>
-                </tr>
-                {AddressTable}
-            </tbody>
-            </table>
-        )
+    return result
+  },
+  getInitialState: function () {
+    return {
+      page: 1,
+      pageSize: 5,
+      pageSizeDefaults: [
+        5,
+        10,
+        25,
+        50,
+        75,
+        100
+      ],
+      numberOfPages: 20
     }
-})
+  },
+  updateNumberOfPages: function () {
+    this.setState( { numberOfPages: numberOfPages } )
+  },
+  render: function () {
+    var addresses = this.populateAddresses( this.props.addresses )
+    return (
+    <section>
+      <div className="cb">
+        <Button
+                className="button fl mr"
+                name="<< Previous"
+                handleClick={ this.handlePageChange.bind(null, -1) } />
+        <p className="count">
+          Page
+          { this.state.page } of
+          { this.state.numberOfPages }
+        </p>
+        <Button
+                className="button fl mr"
+                name="Next >>"
+                handleClick={ this.handlePageChange.bind(null, 1) } />
+        <AddressPageDropdown
+                             pageSizeDefaults={ this.state.pageSizeDefaults }
+                             handleChange={ this.handlePageSizeChange }
+                             page={ this.state.page }
+                             className="fl mr cr" />
+      </div>
+      <div className="cb">
+        <AddressList addresses={ addresses } />
+      </div>
+    </section>
+    )
+  }
+} )
 
-var AddressItem = React.createClass({
-    getDefaultProps: function(){
-        return {
-            address: {
-              City: "Loading...",
-              FirstName: "Loading...",
-              LastName: "Loading...",
-              State: "Loading...",
-              Street: "Loading...",
-              Zip: "Loading..."
-            }
-        }
-    },
-    render: function(){
-        return (
-          <tr><td>{this.props.address.FirstName}</td>
-              <td>{this.props.address.LastName}</td>
-              <td>{this.props.address.Street}</td>
-              <td>{this.props.address.State}</td>
-              <td>{this.props.address.Zip}</td>
-          </tr>
-        )
+// Renders Button. Controls page of addresses.
+var Button = React.createClass( {
+  render: function () {
+    return (
+    <a
+       href="#"
+       className={ this.props.className }
+       onClick={ this.props.handleClick }>
+      { this.props.name }
+    </a>
+    )
+  }
+} )
+
+// Renders Address page dropdown. Controls number of addresses displayed at once.
+var AddressPageDropdown = React.createClass( {
+  getDefaultProps: function () {
+    return { pageSizeDefaults: null, page: null }
+  },
+  render: function () {
+    var options = this.props.pageSizeDefaults.map( function ( value, index ) {
+      return <option
+                     value={ value }
+                     key={ index }>
+               { value } items
+             </option>
+    } )
+
+    return (
+    <div>
+      <select
+              onChange={ this.props.handleChange }
+              className={ this.props.className }>
+        { options }
+      </select>
+    </div>
+    )
+  }
+} )
+
+// Renders List of Addresses
+var AddressList = React.createClass( {
+  getDefaultProps: function () {
+    return {
+      addresses: []
     }
-})
+  },
+  render: function () {
+    var AddressTable = this.props.addresses.map( function ( address, index ) {
+
+      return <AddressItem
+                          address={ address }
+                          key={ index } />
+    } )
+    return (
+    <table>
+      <tbody>
+        <tr>
+          <th>
+            First Name
+          </th>
+          <th>
+            Last Name
+          </th>
+          <th>
+            Street
+          </th>
+          <th>
+            State
+          </th>
+          <th>
+            Zip
+          </th>
+        </tr>
+        { AddressTable }
+      </tbody>
+    </table>
+    )
+  }
+} )
+
+// Renders individual Addresses
+var AddressItem = React.createClass( {
+  getDefaultProps: function () {
+    return {
+      address: {
+        City: "Loading...",
+        FirstName: "Loading...",
+        LastName: "Loading...",
+        State: "Loading...",
+        Street: "Loading...",
+        Zip: "Loading..."
+      }
+    }
+  },
+  render: function () {
+    return (
+    <tr>
+      <td>
+        { this.props.address.FirstName }
+      </td>
+      <td>
+        { this.props.address.LastName }
+      </td>
+      <td>
+        { this.props.address.Street }
+      </td>
+      <td>
+        { this.props.address.State }
+      </td>
+      <td>
+        { this.props.address.Zip }
+      </td>
+    </tr>
+    )
+  }
+} )
 
 
 
 
 
-React.render(<AddressContainer data="/api/address" />, document.getElementById('app'))
+React.render( <AddressContainer data="/api/address" />, document.getElementById( 'app' ) )
